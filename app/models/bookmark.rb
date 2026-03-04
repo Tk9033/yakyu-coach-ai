21daeada003187d8304ffcb92d0c13b0c2b6e925
Tk9033 <koday021369@gmail.com>
2026-03-04 21:46:42 +0900

    ブックマーク機能のアソシエーションと重複防止バリデーションを追加

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,6 @@
 class Bookmark < ApplicationRecord
   belongs_to :user
   belongs_to :ai_result
+
+  validates :user_id, uniqueness: { scope: :ai_result_id }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,9 @@ class User < ApplicationRecord
          :validatable,
          :omniauthable, omniauth_providers: [:google_oauth2]
 
+  has_many :bookmarks, dependent: :destroy
+  has_many :bookmarked_ai_results, through: :bookmarks, source: :ai_result
+
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
