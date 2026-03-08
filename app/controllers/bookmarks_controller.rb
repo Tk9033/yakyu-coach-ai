8d83c376396e46dbb9bb81bb24c5ca3b67e4e912
Tk9033 <koday021369@gmail.com>
2026-03-08 15:12:47 +0900

    ブックマーク機能にフラッシュメッセージを追加

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -12,6 +12,8 @@ class BookmarksController < ApplicationController
     @ai_result = AiResult.find(params[:ai_result_id])
     current_user.bookmarks.create(ai_result: @ai_result)
 
+    flash.now[:notice] = "ブックマークしました"
+
     respond_to do |format|
       format.html { redirect_back_or_to(root_path) }
       format.turbo_stream # create.turbo_stream.erb を探しにいく
@@ -23,6 +25,8 @@ class BookmarksController < ApplicationController
     bookmark = current_user.bookmarks.find(params[:id])
     @ai_result = bookmark.ai_result
     bookmark.destroy
+
+    flash.now[:alert] = "ブックマークを解除しました"
 
     respond_to do |format|
       format.html { redirect_back_or_to(root_path) }
