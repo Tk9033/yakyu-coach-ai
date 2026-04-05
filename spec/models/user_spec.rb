0569ea470b0691d59c5f5b79fe3e1c83f6cfd80b
Tk9033 <koday021369@gmail.com>
2026-04-05 13:18:37 +0900

    Rubocop対応とインデント修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,25 +3,25 @@ require "rails_helper"
 RSpec.describe User, type: :model do
   describe "validations" do
     context "when provider と uid がある場合" do
-  it "有効である" do
-    expect(build(:user)).to be_valid
-  end
-end
+      it "有効である" do
+        expect(build(:user)).to be_valid
+      end
+    end
 
-context "when provider と uid が両方ない場合（ゲスト）" do
-  it "有効である" do
-    expect(build(:user, provider: nil, uid: nil)).to be_valid
-  end
-end
+    context "when provider と uid が両方ない場合（ゲスト）" do
+      it "有効である" do
+        expect(build(:user, provider: nil, uid: nil)).to be_valid
+      end
+    end
 
-context "when provider と uid の組み合わせが重複する場合" do
-  it "無効である" do
-    create(:user, provider: "google_oauth2", uid: "dup_uid")
-    user = build(:user, provider: "google_oauth2", uid: "dup_uid")
+    context "when provider と uid の組み合わせが重複する場合" do
+      it "無効である" do
+        create(:user, provider: "google_oauth2", uid: "dup_uid")
+        user = build(:user, provider: "google_oauth2", uid: "dup_uid")
 
-    expect(user).not_to be_valid
-  end
-end
+        expect(user).not_to be_valid
+      end
+    end
 
   end
 end
